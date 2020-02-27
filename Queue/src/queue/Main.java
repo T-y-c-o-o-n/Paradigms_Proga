3cb889c9e0dc59e1e3e2ec87800b0523e5f19baa
Tycoon <mosson2001@gmail.com>
2020-02-27 03:36:21 +0300

    my tests

--- Queue/src/queue/Main.java
+++ Queue/src/queue/Main.java
@@ -3,21 +3,45 @@ package queue;
 import queue.ArrayQueueModule;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        randomTest(30);
         for (int i = 0; i < 100; ++i) {
-            int type = sc.nextInt();
-            if (type == 0) {
-                System.out.println(ArrayQueueModule.element());
-            }
-            if (type == 1) {
+            String type = sc.next();
+            if (type == "+") {
                 ArrayQueueModule.enqueue(sc.next());
             }
-            if (type == 2) {
+            if (type == "-") {
                 System.out.println(ArrayQueueModule.dequeue());
+            }
+            if (type == "?") {
+                System.out.println(ArrayQueueModule.element());
+            }
+            if (type == "s") {
+                System.out.println(ArrayQueueModule.size());
+            }
+            if (type == "arr") {
+                System.out.println(Arrays.toString(ArrayQueueModule.toArray()));
+            }
+        }
+    }
+
+    public static void randomTest(int count) {
+        Random random = new Random();
+        for (int i = 0; i < count; ++i) {
+            int type = random.nextInt();
+            if (type == 0) {
+                ArrayQueueModule.enqueue(random.nextInt());
+            }
+            if (type == 1) {
+                System.out.println(ArrayQueueModule.dequeue());
+            }
+            if (type == 2) {
+                System.out.println(ArrayQueueModule.element());
             }
             if (type == 3) {
                 System.out.println(ArrayQueueModule.size());
