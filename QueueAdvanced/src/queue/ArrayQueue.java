be4ad7b763e097e62b999b8654eff519a360b22a
Tycoon <mosson2001@gmail.com>
2020-02-27 19:40:13 +0300

    better realization

--- QueueAdvanced/src/queue/ArrayQueue.java
+++ QueueAdvanced/src/queue/ArrayQueue.java
@@ -1,167 +1,77 @@
 package queue;
 
-import java.util.Arrays;
-
 public class ArrayQueue extends AbstractQueue {
     // INV:
+    // Q = {e_first, e_2, e_3, ..., e_n-1, e_last}
     // First in - Last out
-    /*
-    INVs реализации:
-    capacity > 0
-    0 <= head, tail < capacity
-    size == (head - tail) % capacity (% математический!!)
-    if size > 0:
-        elements[head] - first element;
-        elements[(tail - 1) % capacity] - last element; (% математический!!)
-    */
-    private int cap, size;
-    private int head, tail;
-    private Object[] elements;
+    private int head = 0, tail = 0;
+    private Object[] elements = new Object[2];
 
-    public ArrayQueue(int capacity) {
-        cap = capacity;
-        size = 0;
-        head = 0;
-        tail = 0;
-        elements = new Object[capacity];
-    }
-    
-    public ArrayQueue() {
-        this(2);
-    }
-    
-    // Pre: true
+    // Pre: e not null
     // Post: Q' = {e_1, e_2, ..., e_n, e} && |Q| > 0
-    public void enqueue(Object e) {
+    public void enqueue(/*ArrayQueue this, */Object e) {
         assert e != null;
         elements[tail] = e;
         tail = inc(tail);
-        size++;
-        if (size == cap) {
+        if (size() == elements.length) {
             increaseCapacity();
         }
     }
 
     // Pre: |Q| > 0
     // Post: R = e_1 && Q' = {e_2, ..., e_n} && |Q'| = |Q| - 1
-    public Object dequeue() {
-        assert size > 0;
+    public Object dequeue(/*ArrayQueue this*/) {
+        assert size() > 0;
         Object result = elements[head];
         elements[head] = null;
         head = inc(head);
-        size--;
-        if (size * 4 == cap) {
-            decreaseCapacity();
-        }
         return result;
     }
 
     // Pre: |Q| > 0
     // Post: R = e_1
-    public Object element() {
-        assert size > 0;
+    public Object element(/*ArrayQueue this*/) {
+        assert size() > 0;
         return elements[head];
     }
 
     // Pre: true
     // Post: R = |Q|
-    public int size() {
-        return size;
+    public int size(/*ArrayQueue this*/) {
+        if (head == tail) {
+            return elements[head] == null ? 0 : elements.length;
+        }
+        return head > tail ? elements.length - (head - tail) : tail - head;
     }
 
     // Pre: true
     // Post: R = (|Q| == 0)
-    public boolean isEmpty() {
-        return size == 0;
+    public boolean isEmpty(/*ArrayQueue this*/) {
+        return size() == 0;
     }
 
     // Pre: true
     // Post: |Q| == 0
-    public void clear() {
-        cap = 2;
-        size = 0;
+    public void clear(/*ArrayQueue this*/) {
         head = 0;
         tail = 0;
-        elements = new Object[cap];
+        elements = new Object[2];
     }
 
-    // Pre: true
-    // Post: R = [e_1, e_2, ..., e_n-1, e_n]
-    public Object[] toArray() {
-        Object[] result = new Object[size];
-        for (int i = 0; i < size; i++) {
-            result[i] = elements[(head + i) % cap];
-        }
-        return result;
-    }
-
-    // Pre: true
-    // Post: R = "[e_1, e_2, ..., e_n-1, e_n]"
-    public String toStr() {
-        StringBuilder sb = new StringBuilder("[");
-        if (size > 0) {
-            sb.append(elements[head]);
-        }
-        for (int i = 1; i < size; i++) {
-            sb.append(", ");
-            sb.append(elements[(head + i) % cap]);
-        }
-        sb.append(']');
-        return sb.toString();
-    }
-
-    // Pre: true
-    // Post: Q' = {e, e_1, e_2, ..., e_n-1, e_n} && |Q| > 0
-    public void push(Object e) {
-        assert e != null;
-        head = dec(head);
-        elements[head] = e;
-        size++;
-        if (size == cap) {
-            increaseCapacity();
-        }
-    }
-
-    // Pre: |Q| > 0
-    // Post: R = e_n && Q' = {e_1, e_2, ..., e_n-1} && |Q'| = |Q| - 1
-    public Object remove(/*ArrayQueueADT this*/) {
-        assert size > 0;
-        tail = dec(tail);
-        Object result = elements[tail];
-        elements[tail] = null;
-        size--;
-        if (size * 4 == cap) {
-            decreaseCapacity();
-        }
-        return result;
-    }
-
-    // Pre: |Q| > 0
-    // Post: R = e_n
-    public Object peek() {
-        assert size > 0;
-        return elements[dec(tail)];
-    }
-
-    private void increaseCapacity() {
-        elements = Arrays.copyOf(toArray(), cap * 2);
-        cap *= 2;
+    private void increaseCapacity(/*ArrayQueue this*/) {
+        Object[] increased = new Object[elements.length * 2];
+        System.arraycopy(elements, head, increased, 0, elements.length - head);
+        System.arraycopy(elements, 0, increased, elements.length - head, tail);
+        elements = increased;
         head = 0;
-        tail = size;
+        tail = elements.length / 2;
     }
 
-    private void decreaseCapacity() {
-        elements = Arrays.copyOf(toArray(), cap / 2);
-        cap /= 2;
-        head = 0;
-        tail = size;
+    private int inc(/*ArrayQueue this, */int a) {
+        return (a + 1) % elements.length;
     }
 
-    private int inc(int a) {
-        return (a + 1) % cap;
-    }
-
-    private int dec(int a) {
-        return (cap + a - 1) % cap;
+    private int dec(/*ArrayQueue this, */int a) {
+        return (elements.length + a - 1) % elements.length;
     }
 }
